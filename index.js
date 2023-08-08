a02b352db75181bb940f22771975ddb3accbec43
Artur Schwarz <arturschwarzmail@gmail.com>
2023-08-08 10:09:35 +0200

    Fix cors

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const app = express()
 const db = require('@cyclic.sh/dynamodb')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  next();
-});
+app.use(cors())
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
